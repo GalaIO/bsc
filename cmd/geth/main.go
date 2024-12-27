139e9e09b45af41e821236639d6f2a3369dae892
galaio <galaio@users.noreply.github.com>
2024-12-27 15:34:58 +0800

    flags: deprecate pruneancient flag, change to blockHistory;

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -134,7 +134,7 @@ var (
 		utils.MultiDataBaseFlag,
 		utils.PersistDiffFlag,
 		utils.DiffBlockFlag,
-		utils.PruneAncientDataFlag,
+		utils.PruneAncientDataFlag, // deprecated
 		utils.CacheLogSizeFlag,
 		utils.FDLimitFlag,
 		utils.CryptoKZGFlag,
